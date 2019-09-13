c2040a1e6449c3878943f8fe30cfb9cc9e7524f6
Jared Rodriguez <jaredarodriguez@gmail.com>
2019-09-12 22:22:33 -0500

    admin.py updated with photo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Bird, Location 
+from .models import Bird, Location, Photo
 
 # Register your models here.
 
 admin.site.register(Bird)
 admin.site.register(Location)
+admin.site.register(Photo)
